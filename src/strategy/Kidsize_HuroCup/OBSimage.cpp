5abaeb5e7a3e74ea1069061c0816e4c01f494e70
howard-enhao <howardchung29@gmail.com>
2021-05-13 22:19:07 +0800

    have to fix WR = WL && Dy D,XC,XB,DX = DONE

--- src/strategy/Kidsize_HuroCup/OBSimage.cpp
+++ src/strategy/Kidsize_HuroCup/OBSimage.cpp
@@ -1,9 +1,5 @@
 #include "strategy/OBSimage.h"
 
-int totalL = 0;
-int totalR = 0;
-int Left[32]= {1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,3,3,3,3,3,3,3,3,4,4,4,4,4,4,4,4};
-int Right[32]={4,4,4,4,4,4,4,4,3,3,3,3,3,3,3,3,2,2,2,2,2,2,2,2,1,1,1,1,1,1,1,1};
 int main(int argc, char** argv)
 {
 	ros::init(argc, argv, "OBSimage");
@@ -24,141 +20,145 @@ int main(int argc, char** argv)
 }
 
 void OBSimage::strategymain()
-{
-    int FocusMatrix[32] = {10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10}; //攝影機內之焦點矩陣
-    /*int FocusMatrix_R[32] = {4, 4, 4, 4, 4, 4, 4, 4, 3, 3, 3, 3, 3, 3, 3, 3, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1};
-    int FocusMatrix_L[32] = {1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4};*/
-    int LeftMove[32]  = { 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  2,  3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 21};
-    int RightMove[32] = {21, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-    
+{   
 	if(strategy_info->getStrategyStart())
-	{	
-            
-            //cv::Mat compressimage(24,32,CV_8UC3);
+    {	
+        //cv::Mat compressimage(24,32,CV_8UC3);
 
-            cv::Mat image = strategy_info->cvimg->image;
+        cv::Mat image = strategy_info->cvimg->image;
 
-            for(int compress_width = 0 ; compress_width < IMAGEWIDTH  ; compress_width++)
+        for(int compress_height = 0 ; compress_height < IMAGEHEIGHT  ; compress_height++)   //顯示24X32中有沒有障礙物
+        {
+            for(int compress_width = 0 ; compress_width < IMAGEWIDTH ; compress_width++)
             {
-                DeepMatrix_cnt[compress_width] = 0;
-                for(int compress_height = IMAGEHEIGHT - 1 ; compress_height > -1 ; compress_height--)
+                bValue = (image.data + ((compress_height*IMAGEWIDTH + compress_width) * 3 + 0));
+                gValue = (image.data + ((compress_height*IMAGEWIDTH + compress_width) * 3 + 1));
+                rValue = (image.data + ((compress_height*IMAGEWIDTH + compress_width) * 3 + 2));
+                /*if((*rValue == 128 && *gValue == 0 && *bValue == 128) || (*rValue == 0 && *gValue == 128 && *bValue == 128))
                 {
-                    bValue = (image.data + ((compress_height*IMAGEWIDTH + compress_width) * 3 + 0));
-                    gValue = (image.data + ((compress_height*IMAGEWIDTH + compress_width) * 3 + 1));
-                    rValue = (image.data + ((compress_height*IMAGEWIDTH + compress_width) * 3 + 2));
-
-                    if((*rValue == 128 && *gValue == 0 && *bValue == 128) || (*rValue == 0 && *gValue == 128 && *bValue == 128))
-                    {
-                        //DeepMatrix_cnt[compress_width]++;
-						if((compress_height - 1) > -1)
-						{
-							bValue = (image.data + (((compress_height - 1)*IMAGEWIDTH + compress_width) * 3 + 0));
-                        	gValue = (image.data + (((compress_height - 1)*IMAGEWIDTH + compress_width) * 3 + 1));
-                        	rValue = (image.data + (((compress_height - 1)*IMAGEWIDTH + compress_width) * 3 + 2));
-                        	if((*rValue == 128 && *gValue == 0 && *bValue == 128) || (*rValue == 0 && *gValue == 128 && *bValue == 128))
-                        	{
-                            //DeepMatrix_cnt[compress_width]++;
-								if((compress_height - 2) > -1)
-								{
-									bValue = (image.data + (((compress_height - 2)*IMAGEWIDTH + compress_width) * 3 + 0));
-                        			gValue = (image.data + (((compress_height - 2)*IMAGEWIDTH + compress_width) * 3 + 1));
-                       				rValue = (image.data + (((compress_height - 2)*IMAGEWIDTH + compress_width) * 3 + 2));
-                        			if((*rValue == 128 && *gValue == 0 && *bValue == 128) || (*rValue == 0 && *gValue == 128 && *bValue == 128))
-                        			{
-                            			DeepMatrix_cnt[compress_width] = (IMAGEHEIGHT - 1) - compress_height;
-										break;
-                        			}
-								}
-								else
-								{
-									DeepMatrix_cnt[compress_width] = 22;
-									break;
-								}
-                        	}
-						}
-						else
-						{
-							DeepMatrix_cnt[compress_width] = 23;
-							break;
-						}
-                    }
-					if(compress_height == 0)
-					{
-						DeepMatrix_cnt[compress_width] = 24;
-					}
+                    printf("1 ");
                 }
-            }
-
-
-			for(int compress_height = 0 ; compress_height < IMAGEHEIGHT  ; compress_height++)
-            {
-                for(int compress_width = 0 ; compress_width < IMAGEWIDTH ; compress_width++)
+                else
                 {
-                    bValue = (image.data + ((compress_height*IMAGEWIDTH + compress_width) * 3 + 0));
-                    gValue = (image.data + ((compress_height*IMAGEWIDTH + compress_width) * 3 + 1));
-                    rValue = (image.data + ((compress_height*IMAGEWIDTH + compress_width) * 3 + 2));
-					if((*rValue == 128 && *gValue == 0 && *bValue == 128) || (*rValue == 0 && *gValue == 128 && *bValue == 128))
-					{
-						printf("1 ");
-					}
-					else
-					{
-						printf("0 ");
-					}
-				}
-                printf("\n");
-			}
-            printf("\n"),printf("V = ");
-            totalL = 0;
-            totalR = 0;
-
-
-            for (int i = 0; i < DeepMatrixSize; i++) //計算V    
-            {
-                if (FocusMatrix[i] - DeepMatrix_cnt[i] > 0)
-                {                                      
-                    FilterMatrix[i] = FocusMatrix[i] - DeepMatrix_cnt[i]; 
-                    printf("%d,",FilterMatrix[i]);
-                }
-                else 
-                {
-                    FilterMatrix[i] = 0; 
-                    printf("%d,",FilterMatrix[i]);
-                }
-            }
-            printf("\n"),printf("D = ");
-
-            
-            for(int i = 0; i < 32 ;i++)   
-            {
-                deepmatrix.DeepMatrix.push_back(DeepMatrix_cnt[i]);
-                printf("%d,",DeepMatrix_cnt[i]);
-                //printf("%d,",deepmatrix.DeepMatrix[i]);
-                    totalL += DeepMatrix_cnt[i]*DeepMatrix_cnt[i];
-                    totalR += DeepMatrix_cnt[i]*DeepMatrix_cnt[i];
+                    printf("0 ");
+                }*/
             }
             printf("\n");
+        }
+        
+//////////////////////////////////////顯示24X32中有沒有障礙物/////////////////////////////////////////////
+
+        printf("\n"),printf("D = ");                                //計算D && Dy
+        for(int compress_width = 0 ; compress_width < IMAGEWIDTH  ; compress_width++)   
+        {
+            DeepMatrix[compress_width] = 0;
+            Dy = DeepMatrix[0];
+            for(int i = 0;i < 32 ;i++)
+            {
+                if(DeepMatrix[i] < Dy)
+                {
+                    Dy = DeepMatrix[i];
+                }
+            }
+            for(int compress_height = IMAGEHEIGHT - 1 ; compress_height > -1 ; compress_height--)
+            {
+                bValue = (image.data + ((compress_height*IMAGEWIDTH + compress_width) * 3 + 0));
+                gValue = (image.data + ((compress_height*IMAGEWIDTH + compress_width) * 3 + 1));
+                rValue = (image.data + ((compress_height*IMAGEWIDTH + compress_width) * 3 + 2));
+
+                if((*rValue == 128 && *gValue == 0 && *bValue == 128) || (*rValue == 0 && *gValue == 128 && *bValue == 128))
+                {
+                    DeepMatrix[compress_width] = (IMAGEHEIGHT - 1) - compress_height;
+                    break;
+                }
+                if(compress_height == 0)
+                {
+                    DeepMatrix[compress_width] = 24;
+                }
+            }
+        }
+        
+        for(int i = 0; i < 32 ;i++)
+        {
+            deepmatrix.DeepMatrix.push_back(DeepMatrix[i]);
+            printf("%d,",DeepMatrix[i]);
+        }
+        printf("\n");
+        printf("Dy = %d",Dy);
+
+        DeepMatrix_Publish.publish(deepmatrix);
+        deepmatrix.DeepMatrix.clear();
+        
+//////////////////////////////////////計算D && Dy/////////////////////////////////////////////
+
+        Xc = 0;
+        printf("\n"),printf("V = ");    //計算V && XC
+        for (int i = 0; i < DeepMatrixSize; i++)    
+        {
+            if (FocusMatrix[i] - DeepMatrix[i] > 0)
+            {                                      
+                FilterMatrix[i] = FocusMatrix[i] - DeepMatrix[i]; 
+                printf("%d,",FilterMatrix[i]);
+                Xc_cnt ++ ;
+                Xc_n += i;
+                Xc = Xc_n / Xc_cnt;
+            }
+            else 
+            {
+                FilterMatrix[i] = 0; 
+                printf("%d,",FilterMatrix[i]);
+            }
             
-            //printf("totalL = %d\ntotalR = %d", totalL,totalR);
-            DeepMatrix_Publish.publish(deepmatrix);
-            deepmatrix.DeepMatrix.clear();
-            printf("\n");
+        }
+        printf("\n");
+        printf("Xc = %d",Xc);
+        printf("\n");
 
 
+//////////////////////////////////////計算V && XC/////////////////////////////////////////////            
+        WR = 0;
+        WL = 0;
+        Dx = 0;
+        for(int i = 1; i < 32; i++)
+        {
+            WR += (33-i) * FilterMatrix[i-1];
+            WL += i * FilterMatrix[i-1];
+        }
+        printf("WR = %d",WR);
+        printf("\n");
+        printf("WL = %d",WL);
+        printf("\n");
+        if(WL < WR)
+        {
+            Xb = 0;
+            Dx = abs(Xc - Xb);
+            printf("go go go LLL");
+        }
+        else if(WR < WL)
+        {
+            Xb = 31;
+            Dx = abs(Xc - Xb);
+            printf("go go go RRR");
+        }
+        else
+        {
+            printf("go go go go go go go go ");
+        }
+        printf("\n");
+        //Dx = abs(Xc - Xb);
+        printf("Dx = %d",Dx);
 
-
-
+//////////////////////////////////////計算WR , WL && DX && Xb/////////////////////////////////////////////
 
 ////////////////////////////////////opencv/////////////////////////////////////////////
-            cv::resize(image, publish_image, cv::Size(320, 240),CV_INTER_LINEAR);
-			//cv::imshow("publish_image",publish_image);
-			cv::waitKey(1);
-            msg_compressimage = cv_bridge::CvImage(std_msgs::Header(), "bgr8", publish_image).toImageMsg();
-            pub_colormodel.publish(msg_compressimage);
-            image.release();
+        cv::resize(image, publish_image, cv::Size(320, 240),CV_INTER_LINEAR);
+        //cv::imshow("publish_image",publish_image);
+        cv::waitKey(1);
+        msg_compressimage = cv_bridge::CvImage(std_msgs::Header(), "bgr8", publish_image).toImageMsg();
+        pub_colormodel.publish(msg_compressimage);
+        image.release();
 ////////////////////////////////////opencv////////////////////////////////////////////
-        }
-    
+    }
 	else
 	{
 		
