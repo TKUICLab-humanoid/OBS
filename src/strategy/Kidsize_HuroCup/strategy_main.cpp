97024c136e5d66aebbe44721f7fd43723f7659f0
howard-enhao <howardchung29@gmail.com>
2021-06-30 06:45:43 +0800

    replace strategy.cpp && strategy.h

--- src/strategy/Kidsize_HuroCup/strategy_main.cpp
+++ src/strategy/Kidsize_HuroCup/strategy_main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char **argv)
     ros::NodeHandle nh;
     KidsizeStrategy KidsizeStrategy(nh);
 
-    ros::Rate loop_rate(30);
+    ros::Rate loop_rate(20);
 
     KidsizeStrategy.initparameterpath();
 
@@ -21,25 +21,335 @@ int main(int argc, char **argv)
 
 void KidsizeStrategy::strategymain()
 {
-
-    if (strategy_info->getStrategyStart()) //strategy start
+    if (strategy_info->getStrategyStart()) //策略指撥開啟
     {
+        //ROS_INFO("Speed_Max = %d",Speed_Max);
 
-    }
+        Parameter();
+        if (!Continuous_flag) //起步步態
+        {
+            ROS_INFO("senbdbodyauto");  
+            //ros_com->sendBodySector(4); //動作磁區
+            //tool->Delay(1000);
+            //ros_com->sendBodyAuto(continuous_stay_X, continuous_stay_Y, 0, continuous_stay_T, WalkingMode::ContinuousStep, IMU_continuous); //ros_com->sendBodyAuto(-450, 0, 0,-3, WalkingMode::ContinuousStep,IMU_continuous);
+            //tool->Delay(500);
+            Continuous_flag = true;
+        }
+        //ros_com->sendHeadMotor(HeadMotorID::VerticalID, 1603, 300);
+        //tool->Delay(100);
+        //ros_com->sendHeadMotor(HeadMotorID::HorizontalID, 2047, 300); //頭部馬達刻度（左右）左正右負
+        //tool->Delay(100);
 
-}
 
-void KidsizeStrategy::printinfo()
-{
+
+        if(Dy < Dy_Max)
+        {
+            if(continuousX_Speed > continuous_stay_X)    //直走轉原地
+            {
+                ROS_INFO("continuous_forword_X > Speed_Max");
+                while(continuousX_Speed > continuous_stay_X)
+                {
+                    continuousX_Speed -= 50;
+                    ros_com->sendContinuousValue(continuousX_Speed, continuous_stay_Y, 0, continuous_stay_T,IMU_continuous);
+                    tool->Delay(50);
+                    ROS_INFO(" %d",continuousX_Speed);
+                    strategy_info->get_image_flag = true;   
+                    ros::spinOnce();
+                }
+            }
+
+            /*if (strategy_info->color_mask_subject_cnts[1] || strategy_info->color_mask_subject_cnts[2] != 0)    //BY障礙物
+            {
+                if(Dx > 0)
+                {
+                    while (Dx > 0)
+                    {
+                        
+                    }
                     
-    ROS_INFO("nearest_distance_y = %d",nearest_distance_y);
-    ROS_INFO("x_boundary = %.3lf",x_boundary);
-    ROS_INFO("[0] = %5d,[1] = %5d,[2] = %5d",dirdata[0],dirdata[1],dirdata[2]);
-    ROS_INFO("[3] = %5d,[4] = %5d,[5] = %5d",dirdata[3],dirdata[4],dirdata[5]);
-    ROS_INFO("[6] = %5d,[7] = %5d,[8] = %5d",dirdata[6],dirdata[7],dirdata[8]);
+                }
+                else
+                {
 
-    ROS_INFO("\n");
+                }
+            }
+            else if(strategy_info->color_mask_subject_cnts[5] != 0)                                             //R障礙物
+            {
+                for(int i = 0;i < strategy_info->color_mask_subject_cnts[5]; i++)
+                {
+                    if(strategy_info->color_mask_subject[5][i].size > R_size)
+                    {
+                        RedDoorCase();
+                    }
+                }
+            }*/
+        }
+        else
+        {
+            if(continuousX_Speed < Speed_Max)           //原地轉直走
+            {
+                ROS_INFO("continuous_forword_X < Speed_Max");
+                while(continuousX_Speed < Speed_Max)
+                {
+                    continuousX_Speed += 50;
+                    ros_com->sendContinuousValue(continuousX_Speed, continuous_stay_Y, 0, continuous_stay_T,IMU_continuous);
+                    tool->Delay(50);
+                    ROS_INFO(" %d",continuousX_Speed);
+                    strategy_info->get_image_flag = true;   
+                    ros::spinOnce();
+                }
+            }
+        }
+    }
+        
+    else //策略指撥關閉
+    {
+        if (Continuous_flag)
+        {
+            ROS_INFO("stopppppp");
+            //ros_com->sendBodyAuto(0, 0, 0, 0, WalkingMode::ContinuousStep, IMU_continuous); //關閉連續步態
+            Continuous_flag = false;
+            //tool->Delay(1500);
+        }
+        //ros_com->sendBodySector(5);
+        //tool->Delay(1000);
+        //ros_com->sendBodySector(29);
+        //tool->Delay(1000);
+        
+        Parameter();
+    }
 }
+
+/*void KidsizeStrategy::SlopeCalculate() //計算斜率之副函式
+{
+    //////////////////2//////////////////
+    int compareObs_x = 1000000;
+    int compareObs_ymax = 0;
+    int Slope_base_obs = 0;
+    bool Check_obs_flag = false;
+    //////////////////2//////////////////
+    bool Check_label_model_flag = true;
+    rest_flag = false;
+    int Check_Xmin = 160;
+    int Check_Xmax = 160;
+    int Check_Ymax = 0;
+    int Check_Ymax_another_side = 0;
+    int slope_rand[4];
+    int slope_Y[4];
+    float slope[3];
+    slope_avg = 1000000.0;
+    slope_avg_blue = 1000000.0;
+    if (!in_reddoor_flag)
+    {
+        ///ROS_INFO("not in_reddoor_flag!!");
+        for (int i = 0; i < strategy_info->color_mask_subject_cnts[2]; i++)
+        {
+            ///ROS_INFO("Size_blue = %d",strategy_info->color_mask_subject[2][i].size);
+            if (strategy_info->color_mask_subject[2][i].size > 8000)
+            {
+                Check_obs_flag = true;
+                ///ROS_INFO("Size_blue = %d",strategy_info->color_mask_subject[2][i].size);
+                if (strategy_info->color_mask_subject[2][i].YMax > compareObs_ymax)
+                {
+                    Slope_base_obs = i;
+                    compareObs_ymax = strategy_info->color_mask_subject[2][i].YMax;
+                    ///ROS_INFO("Near the robot!!");
+                    if (abs(160 - strategy_info->color_mask_subject[2][i].X) < compareObs_x)
+                    {
+                        ///ROS_INFO("Near the 160!!");
+                        Slope_base_obs = i;
+                        compareObs_x = abs(160 - strategy_info->color_mask_subject[2][i].X);
+                    }
+                }
+            }
+        }
+        if (Check_obs_flag)
+        {
+            ///ROS_INFO("Check_obs_flag true!!");
+            for (int j = 0; j < 4; j++)
+            {
+                int repeat_cnt = 0;
+                int range = (strategy_info->color_mask_subject[2][Slope_base_obs].XMax - 7) - (strategy_info->color_mask_subject[2][Slope_base_obs].XMin + 7);
+                slope_rand[j] = rand() % range + strategy_info->color_mask_subject[2][Slope_base_obs].XMin;
+                while (1)
+                {
+                    if (repeat_cnt != j)
+                    {
+                        if (slope_rand[j] == slope_rand[repeat_cnt])
+                        {
+                            repeat_cnt = 0;
+                            slope_rand[j] = rand() % range + strategy_info->color_mask_subject[2][Slope_base_obs].XMin;
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+                    repeat_cnt++;
+                }
+            }
+            for (int k = 0; k < 4; k++)
+            {
+                bool flag = true;
+                int Xmax = strategy_info->color_mask_subject[2][Slope_base_obs].XMax;
+                int Ymax = strategy_info->color_mask_subject[2][Slope_base_obs].YMax;
+                int cnt = 0;
+                int labelcnt;
+                while (flag)
+                {
+                    labelcnt = 320 * (Ymax - cnt + 1) + slope_rand[k];
+                    if (strategy_info->label_model[labelcnt] == 0x04)
+                    {
+                        for (int a = 1; a < 4; a++)
+                        {
+                            if (strategy_info->label_model[labelcnt - 320 * a] != 0x04)
+                            {
+                                Check_label_model_flag = false;
+                                break;
+                            }
+                        }
+                        if (Check_label_model_flag)
+                        {
+                            slope_Y[k] = Ymax - cnt;
+                            flag = false;
+                        }
+                        else
+                        {
+                            Check_label_model_flag = true;
+                        }
+                    }
+                    if ((cnt + 1) > Ymax)
+                    {
+                        slope_Y[k] = strategy_info->color_mask_subject[2][Slope_base_obs].YMin;
+                        flag = false;
+                    }
+                    else
+                    {
+                        cnt++;
+                    }
+                }
+            }
+            slope[0] = float(slope_Y[1] - slope_Y[0]) / float(slope_rand[1] - slope_rand[0]);
+            slope[1] = float(slope_Y[2] - slope_Y[1]) / float(slope_rand[2] - slope_rand[1]);
+            slope[2] = float(slope_Y[3] - slope_Y[2]) / float(slope_rand[3] - slope_rand[2]);
+            slope_avg_blue = (slope[0] + slope[1] + slope[2]) / 3;
+        }
+        else
+        {
+            continous_angle_offest = 0;
+        }
+    }
+    else
+    {
+        for (int i = 0; i < strategy_info->color_mask_subject_cnts[5]; i++)
+        {
+            if (strategy_info->color_mask_subject[5][i].size > 3000)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    int repeat_cnt = 0;
+                    int range = (strategy_info->color_mask_subject[5][i].XMax - 10) - (strategy_info->color_mask_subject[5][i].XMin + 10);
+                    slope_rand[j] = rand() % range + strategy_info->color_mask_subject[5][i].XMin;
+                    while (1)
+                    {
+                        if (repeat_cnt != j)
+                        {
+                            if (slope_rand[j] == slope_rand[repeat_cnt])
+                            {
+                                repeat_cnt = 0;
+                                slope_rand[j] = rand() % range + strategy_info->color_mask_subject[5][i].XMin;
+                            }
+                        }
+                        else
+                        {
+                            break;
+                        }
+                        repeat_cnt++;
+                    }
+                }
+                for (int k = 0; k < 4; k++)
+                {
+                    bool flag = true;
+                    int Xmax = strategy_info->color_mask_subject[5][i].XMax;
+                    int Ymax = strategy_info->color_mask_subject[5][i].YMax;
+                    int cnt = 0;
+                    int labelcnt;
+                    while (flag)
+                    {
+                        labelcnt = 320 * (Ymax - cnt + 1) + slope_rand[k];
+                        if (strategy_info->label_model[labelcnt] == 0x20)
+                        {
+                            for (int a = 1; a < 4; a++)
+                            {
+                                if (strategy_info->label_model[labelcnt - 320 * a] != 0x20)
+                                {
+                                    Check_label_model_flag = false;
+                                    break;
+                                }
+                            }
+                            if (Check_label_model_flag)
+                            {
+                                slope_Y[k] = Ymax - cnt;
+                                flag = false;
+                            }
+                            else
+                            {
+                                Check_label_model_flag = true;
+                            }
+                        }
+                        if ((cnt + 1) > Ymax)
+                        {
+                            slope_Y[k] = strategy_info->color_mask_subject[5][i].YMin;
+                            flag = false;
+                        }
+                        else
+                        {
+                            cnt++;
+                        }
+                    }
+                }
+                slope[0] = float(slope_Y[1] - slope_Y[0]) / float(slope_rand[1] - slope_rand[0]);
+                slope[1] = float(slope_Y[2] - slope_Y[1]) / float(slope_rand[2] - slope_rand[1]);
+                slope[2] = float(slope_Y[3] - slope_Y[2]) / float(slope_rand[3] - slope_rand[2]);
+                slope_avg = (slope[0] + slope[1] + slope[2]) / 3;
+                break;
+            }
+        }
+    }
+    if (abs(slope_avg) <= 0.05)
+    {
+        face_to_door = true;
+    }
+    else
+    {
+        face_to_door = false;
+    }
+}
+void KidsizeStrategy::IMUSlope()
+{
+    if (Continuous_flag)
+    {
+        if (4 < strategy_info->getIMUValue().Yaw && strategy_info->getIMUValue().Yaw <= 180)
+        {
+            IMU_slope = strategy_info->getIMUValue().Yaw;
+        }
+        else if (-4 > strategy_info->getIMUValue().Yaw && strategy_info->getIMUValue().Yaw >= -179)
+        {
+            IMU_slope = strategy_info->getIMUValue().Yaw;
+        }
+    }
+    else
+    {
+        if (20 < strategy_info->getIMUValue().Yaw && strategy_info->getIMUValue().Yaw <= 180)
+        {
+            IMU_slope = strategy_info->getIMUValue().Yaw;
+        }
+        else if (-20 > strategy_info->getIMUValue().Yaw && strategy_info->getIMUValue().Yaw >= -179)
+        {
+            IMU_slope = strategy_info->getIMUValue().Yaw;
+        }
+    }
+}*/
 
 void KidsizeStrategy::initparameterpath()
 {
@@ -49,16 +359,34 @@ void KidsizeStrategy::initparameterpath()
     }
     printf("parameter_path is %s\n", parameter_path.c_str());
 }
-
-void KidsizeStrategy::GetDeepMatrix(const strategy::DeepMatrix &msg)
+/*void KidsizeStrategy::load_dirtxt() //first_move讀檔之副函式
 {
-	nearest_distance_y = msg.Dy;
-
-	x_boundary = msg.Dx;
+    fstream fin;
+    string sTmp;
+    char line[100];
+    char path[200];
+    strcpy(path, parameter_path.c_str());
+    strcat(path, "/firstmove.ini");
+    fin.open(path, ios::in);
+    try
+    {
+        dirmap[0] = tool->readvalue(fin, "enable", 1);
+        dirmap[1] = tool->readvalue(fin, "dir", 1);
+        dirmap[2] = tool->readvalue(fin, "time", 1);
+        fin.close();
+    }
+    catch (exception e)
+    {
+    }
+}*/
+void KidsizeStrategy::RedDoorCase()
+{
 
 }
 
-void KidsizeStrategy::readparameter() //步態參數之讀檔
+
+
+void KidsizeStrategy::Parameter() //步態參數之讀檔
 {
     fstream fin;
     string sTmp;
@@ -71,18 +399,35 @@ void KidsizeStrategy::readparameter() //步態參數之讀檔
     try
     {
         fin.getline(temp, sizeof(temp));
-        dirdata[0] = tool->readvalue(fin, "continuous_x_offset", 0);
-        dirdata[1] = tool->readvalue(fin, "continuous_y_offset", 0);
-        dirdata[2] = tool->readvalue(fin, "continuous_theta_offset", 0);
-        dirdata[3] = tool->readvalue(fin, "continuous_x_offset_RIGHT", 0);
-        dirdata[4] = tool->readvalue(fin, "continuous_y_offset_RIGHT", 0);
-        dirdata[5] = tool->readvalue(fin, "continuous_theta_offset_RIGHT", 0);
-        dirdata[6] = tool->readvalue(fin, "continuous_x_offset_LEFT", 0);
-        dirdata[7] = tool->readvalue(fin, "continuous_y_offset_LEFT", 0);
-        dirdata[8] = tool->readvalue(fin, "continuous_theta_offset_LEFT", 0);
+        continuous_stay_X = tool->readvalue(fin, "continuous_stay_X", 0);
+        continuous_stay_Y = tool->readvalue(fin, "continuous_stay_Y", 0);
+        continuous_stay_T = tool->readvalue(fin, "continuous_stay_T", 0);
+        fin.getline(temp, sizeof(temp));
+        continuous_TurnRight_X = tool->readvalue(fin, "continuous_TurnRight_X", 0);
+        continuous_TurnRight_Y = tool->readvalue(fin, "continuous_TurnRight_Y", 0);
+        continuous_TurnRight_T = tool->readvalue(fin, "continuous_TurnRight_T", 0);
+        fin.getline(temp, sizeof(temp));
+        continuous_TurnLeft_X = tool->readvalue(fin, "continuous_TurnLeft_X", 0);
+        continuous_TurnLeft_Y = tool->readvalue(fin, "continuous_TurnLeft_Y", 0);
+        continuous_TurnLeft_T = tool->readvalue(fin, "continuous_TurnLeft_T", 0);
+        fin.getline(temp, sizeof(temp));
+        Dy_Max = tool->readvalue(fin, "Dy_Max", 0);
+        Speed_Max = tool->readvalue(fin, "Speed_Max", 0);
+        BY_size = tool->readvalue(fin, "BY_size", 0);
+        R_size = tool->readvalue(fin, "R_size", 0);
         fin.close();
     }
     catch (exception e)
     {
     }
 }
+void KidsizeStrategy::GetDeepMatrix(const strategy::DeepMatrix &msg) //深度矩陣之副函式
+{
+    for (int i = 0; i < 32; i++)
+    {
+        //DeepMatrixValue[i] = msg.DeepMatrix[i];
+        Dx = msg.Dx;
+        Dy = msg.Dy;
+    }
+}
+
--- src/strategy/include/strategy/strategy_main.h
+++ src/strategy/include/strategy/strategy_main.h
@@ -23,6 +23,76 @@
 
 ros::Subscriber DeepMatrix_subscribe;
 
+/*enum strategy_state
+{
+    P_INIT,
+    P_MATRIX_CALCULATE,
+    P_FIND_WALKINGSTATE,
+    P_FM_TURNHEAD,
+    P_WALKINGGAIT,
+    P_DOOR,
+    P_CRAWL
+};
+
+struct obstacle_data
+{
+    int x;
+    int y;
+    int x_min;
+    int x_max;
+    int y_min;
+    int y_max;
+    int size;
+};
+
+enum walking_command
+{
+    FORWARD_FAST,
+    FORWARD_NORMAL,
+    FORWARD_SLOW,
+    RMOVE_FAST,
+    RMOVE_NORMAL,
+    RMOVE_SLOW,
+    LMOVE_FAST,
+    LMOVE_NORMAL,
+    LMOVE_SLOW,
+	DIRmap_RIGHT,
+	DIRmap_LEFT,
+    WC_BIGLEFT,
+    WC_MIDLEFT,
+    WC_LEFT,
+    WC_BIGRIGHT,
+    WC_MIDRIGHT,
+    WC_RIGHT,
+    continuousValue_Ry,
+    continuousValue_Ly,
+	continuousValue_Rt,////add
+	continuousValue_Lt,////add
+	continuousValue_R2t,////add
+	continuousValue_L2t,////add
+	RMOVE_DOOR,
+	LMOVE_DOOR
+};
+
+struct label_model_coordinates
+{
+        float x, y;
+};
+
+
+enum direction
+{
+    Right,
+    Left
+};
+
+enum Head_direction
+{
+    RHD_Center,
+    RHD_Right,
+    RHD_Left
+};*/
+
 using namespace std;
 
 class KidsizeStrategy 
@@ -36,42 +106,173 @@ public:
 		ros_com = RosCommunicationInstance::getInstance();
 	};
 	~KidsizeStrategy(){};
-	void strategymain();
 
 	RosCommunicationInstance *ros_com;
 	ToolInstance *tool;
 	StrategyInfoInstance *strategy_info;
 
-	//////////////////////////////////////new strategy parameter ////////////////////////////////////////////
-	
-	/***************subscribe deepmatrix parameter******************/
+	/*strategy_state m_state;
+	walking_command spec_RLmove_state;
+	walking_command walking_state;
+	walking_command pre_walking_state;
+	obstacle_data obs_data;
+	direction turn_direction;
+	Head_direction head_direction;
+	Head_direction door_direction;
+	label_model_coordinates red_obs_left_coordinates;
+	label_model_coordinates red_obs_right_coordinates;
 
-	int nearest_distance_y = 0;
-	float x_boundary = 0;
-	string parameter_path = "N";
+	vector<obstacle_data> m_obs_vector;
+	vector<obstacle_data> m_finish_obs_vector;
 
-	/******************subscribe deepmatrix parameter******************/
+	ObjectData blue_obs;
+	ObjectData blue_obs_second;
+	ObjectData blue_obs_third;
+	ObjectData P_DOOR_blue;
+	ObjectData P_DOOR_blues;
+	SensorMode IMU_continuous;
+	SensorMode IMU_single;
 
+	int FilterMatrix[32];
+	int FocusMatrix[32];
+	int RMoveValue = 0;
+	int LMoveValue = 0;
+	int true_RMoveValue=0;
+	int true_LMoveValue=0;
+	int compareObs;
+	int compareObssize;
+	int bigobs;
+	int decideforward;
+	int decidemove;
+	int LRmove_cnt;
+	int fixed_LRmove_cnt;
+	int continuousValue_x;
+	int continuous_x_offset;
+	int continuous_y_offset;
+	int continuous_y_offset_RIGHT;
+	int continuous_y_offset_LEFT;
+	int continuous_theta_offset;
+	int continuous_theta_offset_RIGHT;
+	int continuous_theta_offset_LEFT;
+	int continuous_x_offset_RIGHT;
+	int continuous_x_offset_LEFT;
+	int RHead_X;
+	int LHead_X;
+	int insideFMcnt;
+	int first_cnt=0;
+	int Rrightimage=0;
+	int Rleftimage=0;
+	int Ry_fastest;
+	int Ly_fastest;
+   	int dirmap[3];
+	int cntTopYellow_x=0;
+	int cntBottomYellow_x=0;
+	int BottomYellowPoint=0;
+	int MidYellowPoint=0;
+	int TopYellowPoint=0;
+        
+	float slope_avg;         //red door slope
+	float slope_avg_blue;    //blue obs slope
+	float slope;
+	float IMU_slope;
+	float sidelineslope;
 
-	
-	/*********************stategy_main parameter ********************/
-
-
+	bool in_reddoor_flag=false;	//add
+    	bool slope_flag=false;
+	bool Blue_obs_flag = false;
+	bool Red_Door_flag = false;
+	bool first_enter_door = true;
+	bool turnslope_flag = false;
+	bool zero_flag = true;
+	bool check_obs = false;
+	bool checking_obs;
+	bool check_road = false;
+	bool face_to_door = false;
+	bool Center_door = false;
+	bool rest_flag = false;
+	bool pcrawl_flag = false;
+	bool stand_flag = false;
+	bool red_modle_flag = false;
+	bool check_LRmove_flag = false;
+	bool special_obs_flag = false;
+	bool first_continuous_flag = false;
+	bool Turnhead_flag = false;
+	bool turnhead_open_cnt = true;
+	bool second_flag = false;
+	bool check_hole = false;
+	bool door_localization = false;
+	bool first_move_flag =false;
+	bool first_act_flag = true;
+	bool p_door = false;
+	bool movecnt = true;
+	bool First_flag = false;  //第一個藍色障礙物旗標（判斷藍色障礙物在紅門下個數）
+	bool crw_up = false;      //爬的過程中看紅門後的障礙物距離多少,決定要不要爬起
+	bool hole_flag = true;
+	bool sidelinewarning=false;
+	bool rightsidelinewarning=false;
+	bool leftsidelinewarning=false;
+	bool sideline_zero_flag=false;
+	bool twentyflag=false;
+    	int First_width = 0;
+	int angle_offset = 0;
 	int continous_angle_offest = 0;
+	string m_state_string="a";
+	string walking_state_string="a";
+	void facetodoorfun();
+	void FaceToObsFun();	//when face to blue obs
+	void load_dirtxt();
+    	void turnslope();
+	void give_angle();
+	void FaceToFinialLineFun();
+	void traverse();
+	void sideline();
+	void printinfo();*/
 
-	
-	/*********************stategy_main parameter ********************/
 
-	/******************ini  parameter******************/
+	//void SlopeCalculate();
+	//void IMUSlope();
 
-	int dirdata[100];
-	
-	/****************** ini parameter******************/
-	
-	////////////////////////////////////////function/////////////////////////////////////////////
+	//int DeepMatrixValue[32];
 
-	void GetDeepMatrix(const strategy::DeepMatrix &msg);
+	SensorMode IMU_continuous;
+
+
+	bool Continuous_flag = false;
+	int continuousX_Speed = 0;		//前進速度的浮動參數
+
+	float Dx = 0;	//Dx
+	int Dy = 0;	//Dy
+	int WR = 0;	//WR
+	int WL = 0;	//WL
+	int Xc = 0;	//Xc
+	int Xb = 0;	//Xb
+
+
+	int continuous_stay_X = 0;		//步態讀檔
+	int continuous_stay_Y = 0;
+	int continuous_stay_T = 0;
+	int continuous_TurnRight_X = 0;
+	int continuous_TurnRight_Y = 0;
+	int continuous_TurnRight_T = 0;
+	int continuous_TurnLeft_X = 0;
+	int continuous_TurnLeft_Y = 0;
+	int continuous_TurnLeft_T = 0;
+
+	int Dy_Max = 0;
+	int Speed_Max = 0;
+	int BY_size = 0;
+	int R_size = 0;
+
+
+
+	string parameter_path = "N";
+	void strategymain();
 	void initparameterpath();
-	void readparameter();
-	void printinfo();
+	void Parameter();
+	void GetDeepMatrix(const strategy::DeepMatrix &msg);
+	void RedDoorCase();
+
 };
+
+//bool isStart = false;
+
